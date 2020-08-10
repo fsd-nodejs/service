0ca0b4d301b33f6d1d9bcfe653d1d9059f19c6a8
tkvern <tkvern@qq.com>
2020-08-10 10:26:52 +0800

    汉化错误提示

--- src/app/validator/validator.ts
+++ src/app/validator/validator.ts
@@ -1,12 +1,16 @@
+import 'joi-i18n'
 import * as Joi from 'joi'
 import MyError from '@/app/common/my-error'
 
+import locales from './locales'
+
+// 汉化错误提示信息
+Joi.addLocaleData('zh_CN', locales)
 
 export default class Validator {
 
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   protected validate(rule: any, schema: Joi.SchemaLike) {
-    const { error, value } = Joi.validate(rule, schema)
+    const { error, value } = Joi.validate(rule, schema, { locale: 'zh_CN' })
     if (error) {
       throw new MyError('Validation Failed', 422, error.details)
     }
